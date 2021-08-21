29ee228dda796b270c7bffb4a7cc3502ca342070
Aakash Ojha <64247310+theojha01@users.noreply.github.com>
2021-08-21 21:16:49 +0530

    Update task.py

--- task.py
+++ task.py
@@ -1,6 +1,7 @@
 import pyttsx3
 import os
 import time
+import getpass
 
 pyttsx3.speak(" Hey...Welcome to my software")
 x = "Welcome to my software"
@@ -12,7 +13,7 @@ while True :
   print("Press 1 to Install Package \t\t\t\t\t Press 2 to Date")
   print("Press 3 to check list of folder and file \t\t\t Press 4 to Find/Search")
   print("Press 5 to Handling Services \t\t\t\t\t Press 6 to Adding user")
-  print("Press 7 to Create Partition \t\t\t\t\t Press 8 to Mount Disk")
+  print("Press 7 to Create Partition \t\t\t\t\t Press 8 To create a webserver")
   print("Press 9 to setting up different cronjobs \t\t\t Press 10 to Mount Disk")
   print("Press 11 to fstab for mounting \t\t\t\t\t Press 12 To create physical volumes")
   print("Press 13 To create a volume group \t\t\t\t Press 14 to create logical volumes")
@@ -35,3 +36,18 @@ while True :
     option=input("how you want to find")
     file=input("Enter Filename")
     os.system("grep {} {}".format(option, file))
+  
+  elif p=5:
+    service=input("Enter Service Name")
+    os.system("systemctl start {}".format(service)
+
+  elif p=6:
+    name=input("Enter new user name")
+    pass=getpass.getpass()
+    os.system("sudo useradd {}".format(name))
+    os.system("sudo passwd {}".format(name))
+  
+  elif p=7:
+    disk=input("enter your disk name")
+    os.system("sudo fdisk /dev/{}".format(disk))
+    
