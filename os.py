3e872aabed4815e9de8a5efd78e7012a8cd253aa
Aakash Ojha <64247310+theojha01@users.noreply.github.com>
2021-08-26 09:41:05 +0530

    Update os.py

--- os.py
+++ os.py
@@ -1,171 +1,130 @@
-#This is a user friendly software for windows 8 or 8.1
+import webbrowser as wb
 import pyttsx3
-import os
-import time
 
-pyttsx3.speak(" Hey...Welcome to my software")
-x = "Welcome to my software"
-y = x.center(80)
-print(y)
+pyttsx3.speak("Hello")
+print()
+print("WELCOME TO MY INTELLIGENCE WORLD".center(125))                    
+engine.runAndWait()
+pyttsx3.speak("I am your voice assistant")
+pyttsx3.speak("How can i assist you...?")
+
 
 while True:
-	pyttsx3.speak("How may I assist you")
-	print("How may I assist you: ", end='')
-	p=input()
+  print()
+  print("\t \t \t Tell me your requirements")
+  ch=input()
+  
+  if("date" in ch):
+    wb.open("http://192.168.43.202/cgi-bin/newtask.py?x=date")
+    pyttsx3.speak("showing date")
+  elif("calendar" in ch):
+    wb.open("http://192.168.43.202/cgi-bin/newtask.py?x=cal")
+    pyttsx3.speak("showing calendar")
+  elif("IP" in ch )or ("ip address" in ch) or ("address" in ch):
+    wb.open("http://192.168.43.202/cgi-bin/newtask.py?x=ifconfig")
+    pyttsx3.speak("showing IP address")
+  elif("active ports" in ch) or ("ports" in ch):
+    wb.open("http://192.168.43.202/cgi-bin/newtask.py?x=netstat%20-tnlp")
+    pyttsx3.speak("showing active ports")
+  elif("amount" in ch) or ("disc" in ch) and ("space" in ch):
+    wb.open("http://192.168.43.202/cgi-bin/newtask.py?x=df%20-h")
+    pyttsx3.speak("showing amount of disc space")
+  elif("status" in ch) or ("webserver" in ch) and ("apache" in ch) or ("httpd" in ch):
+    wb.open("http://192.168.43.202/cgi-bin/newtask.py?x=systemctl%20status%20httpd")
+    pyttsx3.speak("apache webserver is running")
+  elif("state of selinux" in ch) or ("selinux" in ch):
+    wb.open("http://192.168.43.202/cgi-bin/newtask.py?x=getenforce")
+    pyttsx3.speak("SElinux is permissive")
+  elif("yum" in ch) or ("module" in ch) and ("list" in ch):
+    wb.open("http://192.168.43.202/cgi-bin/newtask.py?x=yum%20module%20list")
+    pyttsx3.speak("Here is the long list of modules")
+  elif("package" in ch) or ("firefox" in ch):
+    wb.open("http://192.168.43.202/cgi-bin/newtask.py?x=yum%20whatprovides%20firefox")
+    pyttsx3.speak("Here is the package name of firefox")
+  elif("ping" in ch) or ("google" in ch) and ("server" in ch):
+    wb.open("http://192.168.43.202/cgi-bin/newtask.py?x=ping%20-c%205%208.8.8.8")
+    pyttsx3.speak("Server is Ping able")
+    
+  #################################AKSHAT CHANGES#############################
+  
+  #install package
+  elif("yum" in ch) or ("module" in ch) and ("install" in ch):
+    packagename=input("Enter package name to install: ")
+    wb.open(f"http://192.168.43.202/cgi-bin/newtask.py?x=yum%20install%20{packagename}%20-y") #check format
+    pyttsx3.speak("Package is installed")
+  
+  ##find file
+  elif("find" in ch) or ("search" in ch) and ("file" in ch):
+    filename=input("Enter file name to search: ")
+    dirpath=input("Enter the location: ")
+    
+    # wb.open(f"http://192.168.43.202/cgi-bin/newtask.py?x=find%20./{dirpath}%20-name%20{filename}")  #if command not worked try 
+    wb.open(f"http://192.168.43.202/cgi-bin/newtask.py?x=find%20{dirpath}%20-name%20{filename}") #check format
+    #pyttsx3.speak("Package is installed")
+    pyttsx3.speak("File Found")
+  
+  ##set cronjobs
+  elif("set" in ch) or ("create" in ch) and ("cronjobs" in ch):
+    command=input("Enter command to run in every two minutes: ")
+    filename=input("Enter the filename to execute: ")
+    commandpass=f"cat; | echo */2 * * * * {command} {filename}"
+    # wb.open(f"http://192.168.43.202/cgi-bin/newtask.py?x=find%20./{dirpath}%20-name%20{filename}")  #if command not worked try 
+    wb.open(f"http://192.168.43.202/cgi-bin/newtask.py?x=crontab%20-l%20|%20{commandpass}|%20crontab%20-") #check format
+    #pyttsx3.speak("Package is installed")
+    pyttsx3.speak("cronjob set")
+  
+  ################################################################################################
+      
 
-	if (("run" in p) or ("launch" in p) or ("open" in p) or ("execute" in p)) and (("chrome" in p) or ("google" in p)):
-		
-		if (("dont" in p) or ("not" in p) or ("don't" in p)):
-			print("", end='')
-		else:
-			pyttsx3.speak("opening  google chrome")
-			os.system("start chrome")
-			time.sleep(3)
-	elif (("run" in p) or ("launch" in p) or ("open" in p) or ("execute" in p)) and (("notepad" in p) or("notebook" in p) or ("editor" in p)):
-		
-		if (("dont" in p) or ("not" in p) or ("don't" in p)):
-			print("", end='')
-		else:
-		
-			pyttsx3.speak("opening notepad")
-			os.system("notepad")
-
-	elif (("run" in p) or ("launch" in p) or ("execute" in p) or ("open" in p)) and (("media" in p) and ("player" in p)):
-		
-		if (("dont" in p) or ("not" in p) or ("don't" in p)):
-			print("", end='')
-		else:
-		
-			pyttsx3.speak("opening windows media player")
-			os.system("start wmplayer")
-
-	elif (("run" in p) or ("launch" in p) or ("execute" in p) or ("open" in p)) and (("explorer" in p) or ("browser" in p)):
-
-		if (("dont" in p) or ("not" in p) or ("don't" in p)):
-			print("", end='')
-		else:
-
-			pyttsx3.speak("opening internet explorer")
-			os.system("start iexplore")
-
-	elif (("run" in p) or ("launch" in p) or ("execute" in p) or ("open" in p)) and (("outlook" in p)):
-		if (("dont" in p) or ("not" in p) or ("don't" in p)):
-			print("", end='')
-		else:
-
-			pyttsx3.speak("opening microsoft outlook")
-			os.system("start outlook")
-
-	elif (("run" in p) or ("launch" in p) or ("execute" in p) or ("open" in p)) and (("powerpoint" in p)):
-		if (("dont" in p) or ("not" in p) or ("don't" in p)):
-			print("", end='')
-		else:
-			pyttsx3.speak("opening microsoft powerpoint")
-			os.system("start powerpnt")
-
-	elif (("run" in p) or ("launch" in p) or ("execute" in p) or ("open" in p)) and (("computer" in p) or ("pc" in p)):
-		if (("dont" in p) or ("not" in p) or ("don't" in p)):
-			print("", end='')
-		else:
-			pyttsx3.speak("opening my computer")
-			os.system("start explorer")
-
-	elif (("run" in p) or ("launch" in p) or ("execute" in p) or ("open" in p)) and (("calculator" in p) or ("calculations" in p)):
-		if (("dont" in p) or ("not" in p) or ("don't" in p)):
-			print("", end='')
-		else:
-			pyttsx3.speak("opening calculator")
-			os.system("calc")
-
-	elif (("run" in p) or ("launch" in p) or ("execute" in p) or ("open" in p)) and (("magnifier" in p) or ("zoom" in p)):
-		if (("dont" in p) or ("not" in p) or ("don't" in p)):
-			print("", end='')
-		else:
-			pyttsx3.speak("opening magnifier")
-			os.system("start magnify")
-
-	elif (("run" in p) or ("launch" in p) or ("execute" in p) or ("open" in p)) and (("paint" in p) or ("draw" in p)):
-		if (("dont" in p) or ("not" in p) or ("don't" in p)):
-			print("", end='')
-		else:
-			pyttsx3.speak("opening paint")
-			os.system("start mspaint")
-
-	elif (("run" in p) or ("launch" in p) or ("execute" in p) or ("open" in p)) and (("registry" in p) and ("editor" in p)):
-		if (("dont" in p) or ("not" in p) or ("don't" in p)):
-			print("", end='')
-		else:
-			pyttsx3.speak("opening registry editor")
-			os.system("start regedit")
-
-	elif (("run" in p) or ("launch" in p) or ("execute" in p) or ("open" in p)) and (("wordpad" in p) or ("word" in p)):
-		if (("dont" in p) or ("not" in p) or ("don't" in p)):
-			print("", end='')
-		else:
-			pyttsx3.speak("opening wordpad")
-			os.system("start write")
-
-	elif (("run" in p) or ("launch" in p) or ("execute" in p) or ("open" in p)) and (("system" in p) and ("configuration" in p)):
-		if (("dont" in p) or ("not" in p) or ("don't" in p)):
-			print("", end='')
-		else:
-			pyttsx3.speak("opening system configuration")
-			os.system("msconfig")
-	
-	elif (("run" in p) or ("launch" in p) or ("execute" in p) or ("open" in p)) and (("command" in p) and ("promt" in p) or ("cmd" in p)):
-		if (("dont" in p) or ("not" in p) or ("don't" in p)):
-			print("", end='')
-		else:
-			pyttsx3.speak("opening command promt")
-			os.system("start cmd.exe")
-
-	elif (("make" in p) or ("open" in p) or ("create" in p)) and (("directory" in p) or ("folder" in p)):
-		if (("dont" in p) or ("not" in p) or ("don't" in p)):
-			print("", end='')
-		else:
-			path = input("Enter thr name of Folder:")
-			pyttsx3.speak("opening path") 
-			os.mkdir("path")
-	elif (("run" in p) or ("launch" in p) or ("execute" in p) or ("open" in p)) and (("whatsapp" in p)):
-		if (("dont" in p) or ("not" in p) or ("don't" in p)):
-			print("", end='')
-		else:
-			pyttsx3.speak("opening whatsapp") 
-			os.system("whatsapp.lnk")
-	elif (("run" in p) or ("launch" in p) or ("execute" in p) or ("open" in p)) and (("tasklist" in p)):
-		if (("dont" in p) or ("not" in p) or ("don't" in p)):
-			print("", end='')
-		else:
-			pyttsx3.speak("opening tasklist") 
-			os.system("tasklist")
-	elif (("run" in p) or ("launch" in p) or ("execute" in p) or ("open" in p)) and (("excel" in p)):
-		if (("dont" in p) or ("not" in p) or ("don't" in p)):
-			print("", end='')
-		else:
-			pyttsx3.speak("opening microsoft excel") 
-			os.system("start excel.exe")
-	elif (("run" in p) or ("launch" in p) or ("execute" in p) or ("open" in p)) and (("control" in p) and ("panel" in p)):
-		if (("dont" in p) or ("not" in p) or ("don't" in p)):
-			print("", end='')
-		else:
-			pyttsx3.speak("opening control panel") 
-			os.system("control panel")
-	elif (("run" in p) or ("launch" in p) or ("execute" in p) or ("open" in p) or ("show" in p)) and (("date" in p)):
-		if (("dont" in p) or ("not" in p) or ("don't" in p)):
-			print("", end='')
-		else:
-			pyttsx3.speak("showing today's date") 
-			os.system("date")
-	elif (("run" in p) or ("launch" in p) or ("execute" in p) or ("open" in p) or ("show" in p)) and (("time" in p)):
-		if (("dont" in p) or ("not" in p) or ("don't" in p)):
-			print("", end='')
-		else:
-			pyttsx3.speak("showing today's time") 
-			os.system("time")		
-
-	elif ("stop" in p) or ("close" in p) or ("exit" in p) or ("end" in p):
-		pyttsx3.speak("closing the software")
-		break
-
-	else:
-		pyttsx3.speak("currently not available")
+  elif ("How" in ch) and ("you can" in ch) or ("help me" in ch):
+    pyttsx3.speak("i can help you in following ways")
+    print()
+    print("!! I can help you in following ways !!".center(126))
+    pyttsx3.speak("such as basic linux commands")
+    print()
+    print(" * Basic Linux Commands".center(126))
+    pyttsx3.speak("Basic Networking")
+    print()
+    print(" * Basic Networking".center(126))
+    pyttsx3.speak("Create User and Restrict User for Specific Program")
+    print()
+    print(" * Create User and Restrict User for Specific Program".center(126))
+    pyttsx3.speak("Configuration of Docker")
+    print()
+    print(" * Configuration of Docker".center(126))
+    print()
+  elif("start service" in ch) or ("start docker service") and ("service") in ch:
+    wb.open("http://192.168.43.202/cgi-bin/newtask.py?x=systemctl%20start%20docker%20")
+    pyttsx3.speak("starting docker service....")
+  elif("verify docker" in ch) and ("installed" in ch):
+    wb.open("http://192.168.43.202/cgi-bin/newtask.py?x=rpm%20-q%20docker-ce")
+    pyttsx3.speak("Now docker is present in your linux server....")
+  elif("how many" in ch) and ("docker containers" in ch) or ("containers" in ch):
+    wb.open("http://192.168.43.202/cgi-bin/newtask.py?x=docker%20ps")
+    pyttsx3.speak("There is no docker containers")
+  elif("how many" in ch) and ("docker images" in ch) or ("images" in ch):
+    wb.open("http://192.168.43.202/cgi-bin/newtask.py?x=docker%20images")
+    pyttsx3.speak("There are so many images")
+  elif("pull" in ch) or ("wordpress" in ch) and ("image" in ch):
+    wb.open("http://192.168.43.202/cgi-bin/newtask.py?x=docker%20pull%20wordpress")
+    pyttsx3.speak("word press image is successfully pulled")
+  elif("launch" in ch) and ("docker container" in ch):
+    wb.open("http://192.168.43.202/cgi-bin/newtask.py?x=docker%20run%20-dit%20wordpress")
+    pyttsx3.speak("word press container is successfully launched")
+  elif("verify" in ch) and ("docker container" in ch) or ("container launched" in ch):
+    wb.open("http://192.168.43.202/cgi-bin/newtask.py?x=docker%20ps")
+    pyttsx3.speak("wordpress container is running....")
+  elif("list" in ch) or ("Cgi" in ch) or ("bin" in ch) :
+    wb.open("http://192.168.43.202/cgi-bin/newtask.py?x=ls")
+    pyttsx3.speak("these are the files present in CGI BIN...")
+  elif("create" in ch) or ("directory" in ch):
+    wb.open("http://192.168.43.202/cgi-bin/newtask.py?x=mkdir%20/namenode")
+    pyttsx3.speak("directory created")
+  elif("SDK" in ch) or ("python library" in ch):
+    wb.open("http://192.168.43.202/cgi-bin/newtask.py?x=pip3%20install%20boto3")
+    pyttsx3.speak("boto three is already satisfied")  
+  elif ("exit" in ch) or ("quit" in ch) or ("bye" in ch):
+    pyttsx3.speak("see you soon,, have a nice day...!!!")
+    break
+  else:
+    print("not understand")
